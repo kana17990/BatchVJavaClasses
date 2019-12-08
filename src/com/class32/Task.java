d9778d71a43fcb7909cd7e2de8c13756f67097b2
kanat17990 <kussenov89@gmail.com>
2019-12-08 14:47:19 -0500

    java Classes

---
+++ src/com/class32/Task.java
@@ -0,0 +1,19 @@
+package com.class32;
+
+import java.util.ArrayList;
+
+public class Task {
+public static void main(String[] args) {
+	 
+	ArrayList<String>arrayList=new ArrayList<String>();
+	arrayList.add("Kairat");
+	arrayList.add("Kanat");
+	arrayList.add("Simbat");
+	arrayList.add("Assel");
+	arrayList.add("Timur");
+	System.out.println(arrayList.isEmpty());
+	System.out.println(arrayList.contains("Simba"));
+	System.out.println(arrayList.size());
+}
+}
+
